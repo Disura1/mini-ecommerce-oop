9f209b79f301905b73e16d930f9fcea7db721d32
Disura1 <disurasandaruwan0@gmail.com>
2026-06-16 16:34:05 +0530

    Initialized Spring Boot project.

---
+++ store-api-oop/src/main/java/com/disura/store_api_oop/StoreApiOopApplication.java
@@ -0,0 +1,13 @@
+package com.disura.store_api_oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApiOopApplication.class, args);
+	}
+
+}
---
+++ store-api-oop/src/test/java/com/disura/store_api_oop/StoreApiOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disura.store_api_oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
